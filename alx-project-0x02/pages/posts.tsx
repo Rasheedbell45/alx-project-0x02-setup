d182b057bd8b7d30dd9bd939277bc2fae42b9a2f
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 05:12:01 +0100

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from 'react';
+import PostCard from '@/components/common/PostCard';
 import Header from '@/components/layout/Header';
+import { PostProps } from '@/interfaces';
 
 export default function PostsPage() {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+      .then((res) => res.json())
+      .then((data) => {
+        const formattedPosts: PostProps[] = data.map((post: any) => ({
+          title: post.title,
+          content: post.body,
+          userId: post.userId,
+        }));
+        setPosts(formattedPosts);
+      });
+  }, []);
+
   return (
     <>
       <Header />
       <main className="p-6">
-        <h1 className="text-2xl font-bold">Posts</h1>
+        <h1 className="text-2xl font-bold mb-4">Latest Posts</h1>
+        {posts.map((post, index) => (
+          <PostCard
+            key={index}
+            title={post.title}
+            content={post.content}
+            userId={post.userId}
+          />
+        ))}
       </main>
     </>
   );
