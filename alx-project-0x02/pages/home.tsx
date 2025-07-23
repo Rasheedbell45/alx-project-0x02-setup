0765b8ca474e3e64b4d3e1d20e47eba8e9e1d6c4
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 04:31:42 +0100

    Update home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const Home: React.FC = () => {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Welcome to the Home Page</h1>
+    </main>
+  );
+};
+
+export default Home;
