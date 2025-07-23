979830e91317df4bc29c0efbe27a3a2a9acdcf05
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 04:34:34 +0100

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,15 @@
+import Link from 'next/link';
+import React from 'react';
 
+const Header: React.FC = () => {
+  return (
+    <header style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
+      <nav>
+        <Link href="/home" style={{ marginRight: '1rem' }}>Home</Link>
+        <Link href="/about">About</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
