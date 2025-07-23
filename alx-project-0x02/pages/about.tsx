1197e9a80678aed39bd990eb9311695c5dd18edd
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 04:54:51 +0100

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Button from '@/components/common/Button';
 
 const About: React.FC = () => {
   return (
-    <main style={{ padding: '2rem' }}>
-      <h1>About Us</h1>
+    <main className="p-8">
+      <h1 className="text-2xl font-bold mb-4">About Page</h1>
+      
+      <div className="flex flex-col gap-4">
+        <Button size="small" shape="rounded-sm">Small Button</Button>
+        <Button size="medium" shape="rounded-md">Medium Button</Button>
+        <Button size="large" shape="rounded-full">Large Button</Button>
+      </div>
     </main>
   );
 };
