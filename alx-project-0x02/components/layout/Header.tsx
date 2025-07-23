6b5a976ec17702d1a637ef450ba7a35415a43503
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 05:07:41 +0100

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,12 +1,19 @@
 import Link from 'next/link';
 import React from 'react';
 
-const Header: React.FC = () => {
+const Header = () => {
   return (
-    <header style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
-      <nav>
-        <Link href="/home" style={{ marginRight: '1rem' }}>Home</Link>
-        <Link href="/about">About</Link>
+    <header className="bg-gray-800 text-white p-4">
+      <nav className="flex space-x-4">
+        <Link href="/home">
+          <span className="hover:underline cursor-pointer">Home</span>
+        </Link>
+        <Link href="/about">
+          <span className="hover:underline cursor-pointer">About</span>
+        </Link>
+        <Link href="/posts">
+          <span className="hover:underline cursor-pointer">Posts</span>
+        </Link>
       </nav>
     </header>
   );
