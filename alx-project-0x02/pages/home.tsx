43f3973c0ea700a82738bec0baa9ef1e8fd7e36b
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 04:40:48 +0100

    Update home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -9,3 +9,20 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
+import React from 'react';
+import Card from '../components/common/Card';
+
+const Home: React.FC = () => {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Welcome to the Home Page</h1>
+      
+      <Card title="Card 1" content="This is the content of card 1." />
+      <Card title="Card 2" content="This card contains different content." />
+      <Card title="Card 3" content="Cards are reusable components." />
+    </main>
+  );
+};
+
+export default Home;
