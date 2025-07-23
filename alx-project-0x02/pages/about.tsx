2e1b8c89dc04e29ec63b62e52d98917a2043561b
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 04:32:58 +0100

    Update about.tsx

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const About: React.FC = () => {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>About Us</h1>
+    </main>
+  );
+};
+
+export default About;
