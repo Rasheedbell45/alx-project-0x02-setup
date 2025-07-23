5fdfa69a054a6ba052de70257dd54c2d8064c7f5
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-23 04:46:18 +0100

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { CardProps } from '../../interfaces';
+import { type CardProps } from '@/interfaces';
 
-const Card: React.FC<CardProps> = ({ title, content }) => {
-  return (
-    <div style={{
-      border: '1px solid #ccc',
-      padding: '1rem',
-      marginBottom: '1rem',
-      borderRadius: '8px',
-      boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)'
-    }}>
-      <h2>{title}</h2>
-      <p>{content}</p>
-    </div>
-  );
-};
+const Card: React.FC<CardProps> = ({ title, content }) => (
+  <div style={{
+    border: '1px solid #ccc',
+    padding: '1rem',
+    marginBottom: '1rem',
+    borderRadius: '8px',
+    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)'
+  }}>
+    <h2>{title}</h2>
+    <p>{content}</p>
+  </div>
+);
 
 export default Card;
